dd408d969357e5e48cbb302c638701f87dcb0d58
Aryan Arora <82714299+Aryan1612@users.noreply.github.com>
2024-07-31 16:02:38 +0530

    Add files via upload

---
+++ merge_sort.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void merge(vector<int> &arr, int low, int mid, int high){
+    int i = low;
+    int j = mid+1;
+    vector<int> ans;
+    while(i <= mid && j <= high){
+        if (arr[i] < arr[j]){
+            ans.push_back(arr[i]);
+            i++;
+        }
+        else{
+            ans.push_back(arr[j]);
+            j++;
+        }
+    }
+    while (i <= mid){
+        ans.push_back(arr[i]);
+            i++;
+    }
+    while (j <= high){
+        ans.push_back(arr[j]);
+        j++;
+    }
+    int m{0};
+    for (int i = low; i <= high; i++){
+        arr[i] = ans[m++];
+    }
+}
+
+void merge_sort(vector<int> &arr, int low, int high){
+    if (low >= high)
+        return;
+    int mid = (low + high)*0.5;
+
+    merge_sort(arr, low, mid);
+    merge_sort(arr, mid+1, high);
+    merge(arr, low, mid, high);
+}
+
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    
+    merge_sort(arr, 0, arr.size()-1);
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+}
